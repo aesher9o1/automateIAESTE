30d7deaa810d75d38cefad4cad5293618dcf6b21
aeser9o1 <aashiskumar986@gmail.com>
2019-02-05 01:37:11 +0530

    added python script

---
+++ iaeste.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  4 15:51:12 2019
+
+@author: aesher9o1
+"""
+
+import pandas as pd
+from docx import *
+import os
+
+filename = ""
+
+array_files=os.listdir()
+for i in array_files:
+    if(".xlsx" in i):
+        filename= i.replace(".xlsx",'')
+        
+        if not os.path.exists("factsheet"):
+            os.makedirs("factsheet")
+
+        xl_file = pd.read_excel(filename+".xlsx")
+        xl_file= xl_file.drop(['Timestamp'], axis=1)
+        xl_file= xl_file.drop(['Passport'], axis=1)
+        
+        
+        
+        "Prepare document for evaluation sheet"
+        document = Document()
+        table = document.add_table(rows=1,cols=7 )
+        table.style = 'TableGrid'
+        hdr_cells = table.rows[0].cells
+        hdr_cells[0].text = 'S.No'
+        hdr_cells[1].text = 'IAESTE ID'
+        hdr_cells[2].text = 'CGPA (out of 20)'
+        hdr_cells[3].text = 'Technical Skills (out of 30)'
+        hdr_cells[4].text = 'Extra Cirricular (out of 20)'
+        hdr_cells[5].text = 'Aptness (out of 20)'
+        hdr_cells[6].text = 'Total'
+        document.add_page_break()
+      
+        
+
+        """Loop to generate factsheet"""
+        for index, row in xl_file.iterrows():
+            document.add_paragraph("IAESTE ID:   " + str(row[0]))
+            document.add_paragraph("Branch:   " + str(row[1]))
+            document.add_paragraph("Number of Backlogs:  " + str(row[2]))
+            document.add_paragraph("Year of Study:   " + str(row[3]))
+            document.add_paragraph("CGPA:  " + str(row[4]))
+            document.add_paragraph("Technical Skills:   " + str(row[5]))
+            document.add_paragraph("Extra-Curricular Activities & Skills:   " + str(row[6]))
+            document.add_paragraph("Aptness:   "+str(row[7]))
+            document.add_page_break()
+            
+            row_cells = table.add_row().cells
+            row_cells[0].text = str(index+1)
+            row_cells[1].text = str(row[0])
+            
+            
+
+        document.save('./factsheet/'+filename+'.docx')
+        
+        
+        
